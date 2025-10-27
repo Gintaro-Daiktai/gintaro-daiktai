c9a4060d13b2e3b1cf4edd1a239602a3ca24b082
Gintaras <gintaras.gaucys1@gmail.com>
2025-10-27 23:36:14 +0000

    feat: connect to db

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -2,9 +2,20 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'postgres',
+      port: 5432,
+      username: 'user',
+      password: 'password',
+      database: 'bank',
+      entities: [],
+      synchronize: true,
+    }),
     ConfigModule.forRoot({
       isGlobal: true,
     }),
