2b49e24703562044ec34dadac18573527ef57b1f
Gintaras <gintaras.gaucys1@gmail.com>
2025-11-01 16:46:35 +0000

    chore: installed needed packages, cleaned up app.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,42 +1,6 @@
-import { useState, useEffect } from "react";
 import "./App.css";
+import { Route } from "react-router";
 
-interface User {
-  name: string;
-  id: string;
-}
-
-function App() {
-  const [users, setUsers] = useState<User[]>([]);
-  const [loading, setLoading] = useState(true);
-
-  useEffect(() => {
-    fetch(import.meta.env.VITE_BACKEND_URL + "/api/users")
-      .then((res) => res.json())
-      .then((data) => {
-        setUsers(data);
-        setLoading(false);
-      })
-      .catch((err) => {
-        console.error("Error fetching users:", err);
-        setLoading(false);
-      });
-  }, []);
-
-  if (loading) return <p>Loading...</p>;
-
-  return (
-    <div>
-      <h1 className="text-xl font-bold">Users</h1>
-      <ul>
-        {users.map((user) => (
-          <li key={user.id} className="p-2 border-b">
-            {user.name}
-          </li>
-        ))}
-      </ul>
-    </div>
-  );
-}
+function App() {}
 
 export default App;
