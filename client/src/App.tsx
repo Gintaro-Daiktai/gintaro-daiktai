edb036d8ed7e0dc68c3d0023f404af2e6e465773
arnas <arnassiusa@gmail.com>
2025-11-03 01:41:07 +0000

    feat: lottery browser, lottery page prototypes.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -9,6 +9,9 @@ import DeliveryStatisticsPage from "./pages/DeliveryStatisticsPage";
 import LotteryStatisticsPage from "./pages/LotteryStatisticsPage";
 import UserStatisticsPage from "./pages/UserStatisticsPage";
 import ProtectedRoute from "./components/ProtectedRoute";
+import ItemPage from "./pages/ItemPage";
+import LotteryPage from "./pages/LotteryPage";
+import LotteriesPage from "./pages/LotteriesPage";
 
 function App() {
   return (
@@ -29,6 +32,9 @@ function App() {
       <Route path="/lotterystats" element={<LotteryStatisticsPage />} />
       <Route path="/userstats" element={<UserStatisticsPage />} />
       <Route path="/browse" element={<BrowsePage />} />
+      <Route path="/lotteries" element={<LotteriesPage />} />
+      <Route path="/lottery/:id" element={<LotteryPage />} />
+      <Route path="/item/:id" element={<ItemPage />} />
     </Routes>
   );
 }
---
+++ client/src/pages/ItemPage.tsx
@@ -0,0 +1,9 @@
+function ItemPage() {
+  return (
+    <div>
+      <h1>Item Page</h1>
+    </div>
+  );
+}
+
+export default ItemPage;
