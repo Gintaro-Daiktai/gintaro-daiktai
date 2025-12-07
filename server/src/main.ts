e2901de8a74a8c5858a1c7a5650ffdb1b582d086
Gintaras <gintaras.gaucys1@gmail.com>
2025-12-07 18:29:38 +0000

    fixed body parser limit for images

--- server/src/main.ts
+++ server/src/main.ts
@@ -3,11 +3,19 @@ import { ConfigService } from '@nestjs/config';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { SeederService } from './database/seeder.service';
+import { json, urlencoded } from 'express';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    bodyParser: true,
+    rawBody: true,
+  });
   app.setGlobalPrefix('api');
   app.useGlobalPipes(new ValidationPipe());
+
+  app.use(json({ limit: '3mb' }));
+  app.use(urlencoded({ extended: true, limit: '3mb' }));
+
   const configService = app.get(ConfigService);
 
   const seeder = app.get(SeederService);
